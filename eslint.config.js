35b5ec225a6f0f268b63a5c543235f1fcd425255
jamesulip <ulip.james@gmail.com>
2024-09-01 13:40:23 +0800

    chore: Update eslint.config.js and App.vue
    - Update eslint.config.js to include a new rule for kebab-case component names in templates
    - Update App.vue to use lowercase 'router-view' instead of 'RouterView'

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,15 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   unocss: true,
   vue: true,
+}, {
+  rules: {
+    'vue/component-name-in-template-casing': [
+      'error',
+      'kebab-case',
+      {
+        registeredComponentsOnly: false,
+        ignores: [],
+      },
+    ],
+  },
 })
