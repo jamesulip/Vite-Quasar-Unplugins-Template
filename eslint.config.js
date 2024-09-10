c306220790c1045ab70df4b520827b21e056cc79
jamesulip <ulip.james@gmail.com>
2024-09-10 17:59:44 +0800

    refactor: Update eslint.config.js to enforce maximum attributes per line in Vue components

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,14 @@ export default antfu({
   vue: true,
 }, {
   rules: {
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 1,
+      },
+      multiline: {
+        max: 1,
+      },
+    }],
     'vue/component-name-in-template-casing': [
       'error',
       'kebab-case',
